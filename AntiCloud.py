cd65105e8890942fe2641e47e2f6155ac0cf9842
HellLords <83832664+HellLords@users.noreply.github.com>
2021-05-09 18:50:00 +0400

    Add files via upload

---
+++ AntiCloud.py
@@ -0,0 +1,34 @@
+import socket
+import sys, os
+
+os.system("clear")
+
+file = open("subdomain.txt")
+content = file.read()
+subdomain = content.splitlines()
+
+print(f"""
+\033[31m\033[01m
+    _          _   _  ____ _                 _ 
+   / \   _ __ | |_(_)/ ___| | ___  _   _  __| |
+  / _ \ | '_ \| __| | |   | |/ _ \| | | |/ _` |\033[01m\033[0m
+ / ___ \| | | | |_| | |___| | (_) | |_| | (_| |
+/_/   \_\_| |_|\__|_|\____|_|\___/ \__,_|\__,_|
+
+
+        .:AntiCloud:. T.ME/HellLords
+
+Под-Доменов в базе : {str(len(subdomain))}
+Введите домен для поиска поддоменов
+Пример: example.com
+""")
+domain = input("[DOMAIN]: ")
+general_ip = socket.gethostbyname(domain)
+print("Сканирование доменов началось! ...")
+print(f"GENERAL-IP: {general_ip}")
+for i in subdomain:
+	try:
+		ip = socket.gethostbyname(f"{i}.{domain}")
+		if ip != general_ip:
+			print(f"SB: {i}.{domain} - {ip}")
+	except:pass
